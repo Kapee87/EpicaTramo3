8be687f203bf1d6fabe8731f3887be99bd8a291b
Kapee87 <kapserker@gmail.com>
2023-11-29 15:31:58 -0300

    fixed render function to home endpoint

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ app.use(cors());
 
 app.use(indexRouter)
 
-app.get("/", res.status(200).send(`
+app.get("/", (req, res) => res.status(200).send(`
     <div
         style="display: flex; justify-content: center; justify-items: center; min-height: 100vh;width: 100%; background-color: bisque;">
         <h1>Api | epicJourney</h1>
